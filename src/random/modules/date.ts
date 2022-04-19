e2c818ab40872ac3c02f42e7983f47657d76fbf1
何喜阳 <iamxiyang@foxmail.com>
2022-04-19 23:19:42 +0800

    feat: 开发中

--- src/random/modules/date.ts
+++ src/random/modules/date.ts
@@ -1,46 +1,8 @@
-// 年
-export const year = (max: number = 22): number => {
-  return Math.floor(Math.random() * max) + 2000
-}
-
-// 月
-export const month = (): number => {
-  return Math.floor(Math.random() * 12) + 1
-}
-
-// 日
-export const date = (): number => {
-  return Math.floor(Math.random() * 30) + 1
-}
-
-// 时
-export const hour = () => {
-  return Math.floor(Math.random() * 24) + 1
-}
-
-// 分
-export const minute = () => {
-  return Math.floor(Math.random() * 60) + 1
-}
-
-// 秒
-export const second = () => {
-  return Math.floor(Math.random() * 60) + 1
-}
+import dayjs from 'dayjs'
 
 // 生成时间戳
 export const timestamp = (): number => {
   return Math.floor(Math.random() * 1000000000000)
-}
-
-// 年月日
-export const YMD = (config: { [key: string]: number } = {}): string => {
-  return `${year(config.year)}-${month()}-${date()}`
-}
-
-// 具体时间
-export const YMDHMS = (config: { [key: string]: number } = {}): string => {
-  return `${year(config.year)}-${month()}-${date()} ${hour()}:${minute()}:${second()}`
 }
 
 // 星期
@@ -49,4 +11,10 @@ export const week = (): number => {
   const strArr: { [index: number]: string } = ['一', '二', '三', '四', '五', '六', '日']
   // @ts-ignore
   return `星期${strArr[number]}`
+}
+
+// TODO 改成随机一个有效时间戳，根据 dayjs 按照一定格式转换返回
+
+export const date = (format = 'Y-M-D') => {
+  return dayjs(timestamp()).format(format)
 }
