698c456889ec1d8f47c23c41e5a0ab2692bbd9ba
何喜阳 <iamxiyang@foxmail.com>
2022-06-09 22:41:50 +0800

    types

--- src/views/edit.vue
+++ src/views/edit.vue
@@ -9,7 +9,8 @@
       <el-tag v-for="cmd in edit.cmds" :key="cmd" :disable-transitions="false" closable @close="delCmd(cmd)">
         {{ cmd }}
       </el-tag>
-      <el-input class="tag-input" v-if="tagInputVisible" ref="tagInputRef" v-model="inputValue" size="small" @keyup.enter="tagInputConfrim" @blur="tagInputConfrim"> </el-input>
+      <el-input class="tag-input" v-if="tagInputVisible" ref="tagInputRef" v-model="inputValue" size="small"
+        @keyup.enter="tagInputConfrim" @blur="tagInputConfrim"> </el-input>
       <el-button v-else-if="edit.cmds.length < 5" size="small" @click="tagInputVisible = true"> + 新唤醒词 </el-button>
     </el-form-item>
     <el-form-item prop="feature" label="快捷启动">
@@ -45,172 +46,172 @@
 </template>
 
 <script setup lang="ts">
-  import { ElInput, ElMessage, FormInstance } from 'element-plus'
-  import variable from '../constant/variable'
-  import { runCmd } from '../utils/random'
-  import { uuid } from '../random'
-  import useAppStore from '../store/index'
+import { ElInput, ElMessage, FormInstance } from 'element-plus'
+import variable from '../constant/variable'
+import { runCmd } from '../utils/random'
+import { uuid } from '../random'
+import useAppStore from '../store/index'
 
-  const appStore = useAppStore()
-  const { features } = $(storeToRefs(appStore))
+const appStore = useAppStore()
+const { features } = $(storeToRefs(appStore))
 
-  const router = useRouter()
+const router = useRouter()
 
-  const defaultEdit = () => {
-    return {
-      code: '',
-      explain: '',
-      cmds: [],
-      content: '',
-      feature: false,
+const defaultEdit = () => {
+  return {
+    code: '',
+    explain: '',
+    cmds: [],
+    content: '',
+    feature: false,
+  }
+}
+
+let id = $ref('')
+let rev = $ref('')
+const defaultData = defaultEdit()
+let edit = $ref<Feature>(defaultData)
+
+// 初始化数据
+onMounted(() => {
+  const { id: queryId } = useRoute().query
+  id = (queryId as string) || `cmd-${uuid()}`
+
+  if (queryId) {
+    const find = features.find((item: DbDoc) => item._id === queryId)
+    const data = find?.data
+    if (data) {
+      rev = find?._rev as string
+      edit = data
     }
   }
+})
 
-  let id = $ref('')
-  let rev = $ref('')
-  const defaultData = defaultEdit()
-  let edit = $ref<Feature>(defaultData)
+// 自定义唤醒词
+let inputValue = $ref('')
+let tagInputVisible = $ref(false)
+let tagInputRef = $ref<InstanceType<typeof ElInput>>()
 
-  // 初始化数据
-  onMounted(() => {
-    const { id: queryId } = useRoute().query
-    id = (queryId as string) || `cmd-${uuid()}`
+const delCmd = (tag: string) => {
+  edit.cmds.splice(edit.cmds.indexOf(tag), 1)
+}
 
-    if (queryId) {
-      const find = features.find((item) => item._id === queryId)
-      const data = find?.data
-      if (data) {
-        rev = find?._rev as string
-        edit = data
-      }
-    }
-  })
-
-  // 自定义唤醒词
-  let inputValue = $ref('')
-  let tagInputVisible = $ref(false)
-  let tagInputRef = $ref<InstanceType<typeof ElInput>>()
-
-  const delCmd = (tag: string) => {
-    edit.cmds.splice(edit.cmds.indexOf(tag), 1)
-  }
-
-  watchEffect(() => {
-    if (tagInputVisible) {
-      nextTick(() => {
-        tagInputRef!.input!.focus()
-      })
-    }
-  })
-
-  const tagInputConfrim = () => {
-    if (inputValue) {
-      edit.cmds.push(inputValue)
-    }
-    tagInputVisible = false
-    inputValue = ''
-  }
-
-  // 内容效验
-  const ruleFormRef = ref<FormInstance>()
-  const validatePass = (rule: any, value: any, callback: any) => {
-    if ((!value || value.length === 0) && edit.feature) {
-      callback(new Error('快捷启动时唤醒词不能为空'))
-    } else {
-      callback()
-    }
-  }
-
-  const formRules = reactive({
-    cmds: [{ trigger: 'blur', validator: validatePass }],
-    content: [{ trigger: 'blur', required: true, message: '指令内容必须填写' }],
-    explain: [{ trigger: 'blur', required: true, message: '指令名称必须填写' }],
-  })
-
-  // 内置变量
-  const _variable = computed(() => {
-    return Object.keys(variable).map((name) => {
-      const { example, description } = variable[name]
-      return {
-        name,
-        example,
-        description,
-      }
+watchEffect(() => {
+  if (tagInputVisible) {
+    nextTick(() => {
+      tagInputRef!.input!.focus()
     })
+  }
+})
+
+const tagInputConfrim = () => {
+  if (inputValue) {
+    edit.cmds.push(inputValue)
+  }
+  tagInputVisible = false
+  inputValue = ''
+}
+
+// 内容效验
+const ruleFormRef = ref<FormInstance>()
+const validatePass = (rule: any, value: any, callback: any) => {
+  if ((!value || value.length === 0) && edit.feature) {
+    callback(new Error('快捷启动时唤醒词不能为空'))
+  } else {
+    callback()
+  }
+}
+
+const formRules = reactive({
+  cmds: [{ trigger: 'blur', validator: validatePass }],
+  content: [{ trigger: 'blur', required: true, message: '指令内容必须填写' }],
+  explain: [{ trigger: 'blur', required: true, message: '指令名称必须填写' }],
+})
+
+// 内置变量
+const _variable = computed(() => {
+  return Object.keys(variable).map((name) => {
+    const { example, description } = variable[name]
+    return {
+      name,
+      example,
+      description,
+    }
   })
-  const addVariable = (val: string) => {
-    edit.content = edit.content + val
-  }
+})
+const addVariable = (val: string) => {
+  edit.content = edit.content + val
+}
 
-  // 数据测试
-  let dialogTest = $ref(false)
-  let testText = $ref<string[] | number[]>([])
+// 数据测试
+let dialogTest = $ref(false)
+let testText = $ref<string[] | number[]>([])
 
-  const testCmd = () => {
-    const text = runCmd(edit.content)
-    const text2 = runCmd(edit.content)
-    testText = [text, text2]
-    dialogTest = true
-  }
+const testCmd = () => {
+  const text = runCmd(edit.content)
+  const text2 = runCmd(edit.content)
+  testText = [text, text2]
+  dialogTest = true
+}
 
-  // 保存指令
-  const saveCmd = async (formEl: FormInstance | undefined) => {
-    try {
-      if (!formEl) return
-      await formEl.validate()
-      // 判断功能名称不能重复
-      for (let i = 0, len = features.length; i < len; i++) {
-        const _id = features[i]._id
-        const { explain } = features[i].data
-        if (_id !== id && explain === edit.explain) {
-          ElMessage.error('功能名称已存在，不能重复')
-          break
-        }
+// 保存指令
+const saveCmd = async (formEl: FormInstance | undefined) => {
+  try {
+    if (!formEl) return
+    await formEl.validate()
+    // 判断功能名称不能重复
+    for (let i = 0, len = features.length; i < len; i++) {
+      const _id = features[i]._id
+      const { explain } = features[i].data
+      if (_id !== id && explain === edit.explain) {
+        ElMessage.error('功能名称已存在，不能重复')
+        break
       }
-      if (!edit.code) {
-        edit.code = id
-      }
-      const index = features.findIndex((item) => item._id === id)
-      if (index >= 0) {
-        features.splice(index, 1, {
-          _id: id,
-          _rev: rev,
-          data: toRaw(edit),
-        })
-      } else {
-        features.push({
-          _id: id,
-          _rev: rev,
-          data: toRaw(edit),
-        })
-      }
-      ElMessage({
-        message: '已保存',
-        type: 'success',
-        center: true,
+    }
+    if (!edit.code) {
+      edit.code = id
+    }
+    const index = features.findIndex((item: DbDoc) => item._id === id)
+    if (index >= 0) {
+      features.splice(index, 1, {
+        _id: id,
+        _rev: rev,
+        data: toRaw(edit),
       })
-      setTimeout(() => {
-        router.back()
-      }, 800)
-    } catch (err) {
-      ElMessage({
-        message: '信息填写不完整',
-        type: 'warning',
-        center: true,
+    } else {
+      features.push({
+        _id: id,
+        _rev: rev,
+        data: toRaw(edit),
       })
     }
+    ElMessage({
+      message: '已保存',
+      type: 'success',
+      center: true,
+    })
+    setTimeout(() => {
+      router.back()
+    }, 800)
+  } catch (err) {
+    ElMessage({
+      message: '信息填写不完整',
+      type: 'warning',
+      center: true,
+    })
   }
+}
 </script>
 
 <style scoped lang="scss">
-  :deep(.el-tag),
-  :deep(.el-button),
-  :deep(.tag-input) {
-    margin-right: 14px;
-  }
+:deep(.el-tag),
+:deep(.el-button),
+:deep(.tag-input) {
+  margin-right: 14px;
+}
 
-  .tag-input {
-    display: inline-block;
-    width: 100px;
-  }
+.tag-input {
+  display: inline-block;
+  width: 100px;
+}
 </style>
