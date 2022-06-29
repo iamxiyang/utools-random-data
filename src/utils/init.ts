e19851b263149dadeca0ec83f24c9a1b27580c2c
何喜阳 <iamxiyang@foxmail.com>
2022-06-29 22:20:07 +0800

    fix: 捕获错误，避免应用白屏

--- src/utils/init.ts
+++ src/utils/init.ts
@@ -6,19 +6,21 @@ export const initCmds = () => {
   if (features.length) {
     // 每次启动的时候都根据配置修改指令，这样尽可能减少版本更新带来的影响。
     features.forEach((item) => {
-      const {
-        data: { code, explain, cmds, feature },
-      } = item
-      if (feature) {
-        utools.setFeature({
-          code,
-          explain,
-          cmds,
-          platform: ['win32', 'darwin', 'linux'],
-        })
-      } else {
-        tryRemoveFeature(item)
-      }
+      try {
+        const {
+          data: { code, explain, cmds, feature },
+        } = item
+        if (feature) {
+          utools.setFeature({
+            code,
+            explain,
+            cmds,
+            platform: ['win32', 'darwin', 'linux'],
+          })
+        } else {
+          tryRemoveFeature(item)
+        }
+      } catch {}
     })
     return
   }
