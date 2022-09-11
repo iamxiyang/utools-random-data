d8cbaf4efb19525d13c61ed2a79b7de41f8b3d40
何喜阳 <iamxiyang@foxmail.com>
2022-09-11 16:15:26 +0800

    fix: 修复变量参数带空格时不能正确匹配问题

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -4,15 +4,16 @@ import { _clearAddressCaches } from '../random'
 // 解析内容
 export const runCmd = (content: string) => {
   if (!content) return ''
-  // 有可能的格式示例： ${日期} ${身份证号} ${随机数()} ${随机数(10)} ${随机数(10,20)}
   let tempCaches: { [key: string]: string } = {}
-  const parseContent = content.replace(/\$\{(([.\S](?!\$))*)\}/gim, (match, p1, offset, string) => {
+  const parseContent = content.replace(/\$\{(.(?!\${))*\}/gim, (match, p1, offset, string) => {
     // ${域名}
     // ${身份证号()}
     // ${文本()}
     // ${文本(10)}
     // ${文本(10,20)}
     // ${正则(/[a-zA-Z0-9]{6,12}/)}
+    // ${日期(YYYY-MM-DD HH:mm:ss)} 
+    // ${日期(YYYY-MM-DDHH:mm:ss)}
     if (tempCaches[match]) {
       // 如果在一个内容中出现相同的变量，则始终返回相同内容
       return tempCaches[match]
