49486c755ca3406797cfa856bf46d33cba2d6cae
何喜阳 <iamxiyang@foxmail.com>
2022-11-26 17:48:34 +0800

    chore: 修改描述

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,30 +1,26 @@
 <template>
   <h2 class="m-y-20px">关于插件</h2>
-  <p>随机生成虚假数据插件，是一款 <strong>开源</strong>、<strong>免费</strong> 的 uTools 插件。该插件的目的是帮助开发、测试人员在开发阶段进行数据测试，所有生成的数据都是虚假的，只确保符合特定校验规则，并不是真实存在的。</p>
-  <p>Github地址：<span class="link" @click="openGithub">https://github.com/iamxiyang/utools-random-data</span></p>
-  <p>开发者微信：hexiyangwxh <span class="note">（添加时请备注来自uTools随机生成数据插件）</span></p>
-  <p>如果这个插件帮到了您，希望能收获到您在插件商店的5分好评，或者在Github的Star，也欢迎您参与代码贡献。</p>
+  <p>随机生成虚假数据插件，是一款 <strong>开源</strong>、<strong>免费</strong> 的 uTools 插件。该插件的目的是帮助开发和测试人员在开发阶段进行数据测试，所有生成的数据都是虚假的，只确保符合特定校验规则，并不是真实存在的。</p>
+  <p>如果这个插件帮到了您，您可以在插件商店打 5 分好评，或者在 Github 点一个 Star，这将是作者持续维护的动力。</p>
+  <p>开源地址<span class="link ml-2" @click="openGithub">https://github.com/iamxiyang/utools-random-data</span></p>
+  <img src="../assets/mp-qrcode.jpg" class="w-200px" />
+  <p class="mt-2px">作者微信公众号</p>
 </template>
 
 <script setup lang="ts">
-import { openUrl } from '../utils/helper'
-const openGithub = () => openUrl('https://github.com/iamxiyang/utools-random-data')
+  import { openUrl } from '../utils/helper'
+  const openGithub = () => openUrl('https://github.com/iamxiyang/utools-random-data')
 </script>
 
 <style scoped lang="scss">
-p {
-  font-size: 16px;
-  line-height: 2;
-  margin-bottom: 10px;
-}
+  p {
+    font-size: 16px;
+    line-height: 2;
+    margin-bottom: 10px;
+  }
 
-.link {
-  color: #3a8ee6;
-  cursor: pointer;
-}
-
-.note {
-  font-size: 14px;
-  color: #888;
-}
+  .link {
+    color: #3a8ee6;
+    cursor: pointer;
+  }
 </style>
