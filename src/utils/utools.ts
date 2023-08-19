e997972592b32defbd15661a6e680e8a14df7a45
何喜阳 <iamxiyang@foxmail.com>
2023-08-19 21:14:13 +0800

    feat: 使用 uTools 内置 api 完成隐藏窗口并粘贴

--- src/utils/utools.ts
+++ src/utils/utools.ts
@@ -18,22 +18,11 @@ let isFirstUse = !utools?.dbStorage?.getItem('is-first-use')
 
 // 执行隐藏窗口、复制粘贴、退出插件步骤
 export const copyPasteOut = (text: string) => {
-  if (!window.utools) {
-    debug('仅uTools插件环境可用', text)
-    return
-  }
-
-  if (window.isDetach) {
-    // TODO 分离窗口状态下无法正常粘贴，给出提示
-  }
-
-  window.utools.hideMainWindow()
-  utools.copyText(text)
-  paste()
+  utools.hideMainWindowPasteText(text)
   if (isFirstUse) {
     showTips()
     utools.dbStorage.setItem('is-first-use', new Date().getTime())
     isFirstUse = false
   }
-  window.utools.outPlugin()
+  utools.outPlugin()
 }
