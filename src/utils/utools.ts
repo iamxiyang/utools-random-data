00bc08108eed95e10edf253fccdb532e5fd4ba70
何喜阳 <iamxiyang@foxmail.com>
2023-08-20 11:01:18 +0800

    fix: 自动粘贴

--- src/utils/utools.ts
+++ src/utils/utools.ts
@@ -18,11 +18,17 @@ let isFirstUse = !utools?.dbStorage?.getItem('is-first-use')
 
 // 执行隐藏窗口、复制粘贴、退出插件步骤
 export const copyPasteOut = (text: string) => {
-  utools.hideMainWindowPasteText(text)
+  window.utools.hideMainWindow()
+  utools.copyText(text)
+  paste()
+
   if (isFirstUse) {
     showTips()
     utools.dbStorage.setItem('is-first-use', new Date().getTime())
     isFirstUse = false
+  } else if (window.isDetach) {
+    utools.showNotification('数据已生成，分离窗口下会影响自动粘贴，请手动粘贴使用，建议关闭分离窗口')
   }
+
   utools.outPlugin()
 }
