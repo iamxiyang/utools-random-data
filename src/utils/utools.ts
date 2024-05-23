5b06925b6d6b785d703130dc429b69cf518240c4
何喜阳 <iamxiyang@foxmail.com>
2024-05-23 22:17:23 +0800

    feat: 使用 utools 新 API，优化生成数据操作体验

--- src/utils/utools.ts
+++ src/utils/utools.ts
@@ -18,17 +18,24 @@ let isFirstUse = !utools?.dbStorage?.getItem('is-first-use')
 
 // 执行隐藏窗口、复制粘贴、退出插件步骤
 export const copyPasteOut = (text: string) => {
-  window.utools.hideMainWindow()
-  utools.copyText(text)
-  paste()
+  if (utools.hideMainWindowPasteText) {
+    utools.hideMainWindowPasteText(text)
+  } else {
+    window.utools.hideMainWindow()
+    utools.copyText(text)
+    paste()
+  }
 
   if (isFirstUse) {
     showTips()
     utools.dbStorage.setItem('is-first-use', new Date().getTime())
     isFirstUse = false
   } else if (window.isDetach) {
+    utools.copyText(text)
     utools.showNotification('数据已生成，分离窗口下会影响自动粘贴，请手动粘贴使用，建议关闭分离窗口')
   }
 
-  utools.outPlugin()
+  if (!window.isDetach) {
+    utools.outPlugin()
+  }
 }
